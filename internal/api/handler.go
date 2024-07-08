ab6ef7ee1a0fcb3a00839e7e4a947c742ba1a307
Aekam <106927980+aekams@users.noreply.github.com>
2024-07-07 19:55:44 -0600

    fix: temp removal of kafka

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"data-collector/internal/kafka"
+	//"data-collector/internal/kafka"
 	"data-collector/internal/model"
 	"net/http"
 
@@ -9,21 +9,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Assuming the producer and topic are initialized globally or passed via context
+// Pending: Producer and topic needs to be initialized globally or passed via context
 // Here, we're using a global variable for simplicity
 var producer *confluentKafka.Producer
 var kafkaTopic string
 
-func init() {
-	var err error
-	// Assuming you have initialized these values from your config system or environment variables
-	config := &kafka.ProducerConfig{BootstrapServers: "localhost:9092"}
-	producer, err = kafka.NewProducer(config)
-	if err != nil {
-		panic("Failed to create Kafka producer")
-	}
-	kafkaTopic = "your-topic" // This should be set based on your application's needs
-}
+// Temp removal of kafka
+// func init() {
+// 	var err error
+// 	//  @TODO Initialize these values from config system or environment variables
+// 	config := &kafka.ProducerConfig{BootstrapServers: "localhost:9092"}
+// 	producer, err = kafka.NewProducer(config)
+// 	if err != nil {
+// 		panic("Failed to create Kafka producer")
+// 	}
+// 	kafkaTopic = "data-topic"
+// }
 
 // PostData handles incoming POST requests and sends data to Kafka
 func PostData(c *gin.Context) {
@@ -34,10 +35,11 @@ func PostData(c *gin.Context) {
 	}
 
 	// Send data to Kafka
-	if err := kafka.SendToKafka(data, producer, kafkaTopic); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+	// Temp removal of kafka
+	// if err := kafka.SendToKafka(data, producer, kafkaTopic); err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
 
 	c.JSON(http.StatusOK, gin.H{"status": "data sent to Kafka"})
 }
